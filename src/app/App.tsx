a6ff8083257f1ba65348e4d3ea5b8ef1ec0db5da
beurmuz <fallinta2@gmail.com>
2025-09-11 00:04:53 +0900

    #2 [styles] Footer 수정

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,12 @@
-// import "./App.css";
-
 import Footer from "../shared/ui/Footer";
 import Header from "../shared/ui/Header";
 
 function App() {
   return (
-    <>
+    <div className="App flex flex-col  justify-between min-h-screen">
       <Header />
       <Footer />
-    </>
+    </div>
   );
 }
 
