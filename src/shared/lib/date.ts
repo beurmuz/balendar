3142e9fc6c1a44acff0ab4a37fca91c24daaa087
beurmuz <fallinta2@gmail.com>
2025-09-17 15:57:52 +0900

    #4 [개선] 특정 달에 따라 길이가 유동적으로 변화하도록 개선하기

--- src/shared/lib/date.ts
+++ src/shared/lib/date.ts
@@ -1,29 +1,31 @@
-import { startOfMonth, endOfMonth, addDays } from "date-fns";
+import { startOfMonth, getDaysInMonth, addDays } from "date-fns";
 
 // 달력의 출발점, 종료점 등 화면에 렌더링 될 기간을 구하는 함수
 export function getMonthGrid(date: Date) {
   const start = startOfMonth(date);
-  const end = endOfMonth(date);
+  const startDay = start.getDay(); // 0: 일 ~ 6: 토
 
-  // 0: 일요일 ~ 6: 토요일, 월요일(1)을 기준으로 앞에 이전 달로 몇 칸을 채워야 하는지 계산
-  const startDay = start.getDay(); // 0~6
   const beforeRestDays = (startDay + 6) % 7; // 월요일(1)을 기준으로 변경
+  const inMonth = getDaysInMonth(start); // 해당 월의 총 일수
+
+  const totalWeeks = Math.ceil((beforeRestDays + inMonth) / 7); // 총 몇 주가 필요한지
+  const totalCells = totalWeeks * 7; // 총 몇 칸이 필요한지
 
   // Grid 시작일 (이전달로 이동한 날짜)
   const gridStart = addDays(start, -beforeRestDays);
 
   // 6주 x 7일 = 42칸 고정
   const days: Date[] = Array.from(
-    { length: 42 },
+    { length: totalCells },
     (_, i) => addDays(gridStart, i) // (시작일, 0~41번째)
   );
 
   const weeks: Date[][] = [];
-  for (let i = 0; i < days.length; i += 7) {
+  for (let i = 0; i < totalCells; i += 7) {
     weeks.push(days.slice(i, i + 7));
   }
 
-  return { start, end, weeks };
+  return { start, weeks };
 }
 
 // 같은 연, 월인지 검사하는 함수
