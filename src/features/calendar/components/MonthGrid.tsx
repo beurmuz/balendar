42973c139719f4b419f8df1b6a984433c95caf3f
beurmuz <fallinta2@gmail.com>
2025-09-17 21:58:50 +0900

    #3 [dev] 주말 폰트 색상 주기 및 에러 해결

--- src/features/calendar/components/MonthGrid.tsx
+++ src/features/calendar/components/MonthGrid.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import { getMonthGrid, isSameMonthAndYear } from "../../../shared/lib/date";
 
 const WEEK_LABELS = ["월", "화", "수", "목", "금", "토", "일"];
@@ -13,8 +14,14 @@ export default function MonthGrid() {
 
       {/* 요일 */}
       <div className="grid grid-cols-7 gap-[1px]">
-        {WEEK_LABELS.map((week) => (
-          <div key={week} className="py-2 text-sm font-medium text-center">
+        {WEEK_LABELS.map((week, idx) => (
+          <div
+            key={week}
+            className={classNames("py-2 text-sm font-medium text-center", {
+              "text-blue-500": idx === 5,
+              "text-red-500": idx === 6,
+            })}
+          >
             {week}
           </div>
         ))}
@@ -24,17 +31,28 @@ export default function MonthGrid() {
       <div className="grid grid-cols-7 gap-[1px]">
         {weeks.flat().map((day, idx) => {
           const inMonth = isSameMonthAndYear(day, start);
+          const weekend = day.getDay();
+
+          // 달력에 표시되는 날짜 색상
+          const colorClass = inMonth
+            ? weekend === 0
+              ? "text-red-500"
+              : weekend === 6
+              ? "text-blue-500"
+              : "text-black"
+            : "text-gray-400";
 
           return (
             <button
               type="button"
               key={idx}
-              className={"min-h-24 grid items-start p-1"}
+              className={"min-h-24 grid items-start justify-items-center p-1"}
             >
               <span
-                className={
-                  inMonth ? "text-xs text-black" : "text-gray-400 text-xs"
-                }
+                className={classNames(
+                  "text-xs flex items-center justify-center w-6 h-6 rounded-full",
+                  colorClass
+                )}
               >
                 {day.getDate()}
               </span>
--- src/shared/lib/date.ts
+++ src/shared/lib/date.ts
@@ -6,9 +6,9 @@ export function getMonthGrid(date: Date) {
   const startDay = start.getDay(); // 0: 일 ~ 6: 토
 
   const beforeRestDays = (startDay + 6) % 7; // 월요일(1)을 기준으로 변경
-  const inMonth = getDaysInMonth(start); // 해당 월의 총 일수
+  const totalDates = getDaysInMonth(start); // 해당 월의 총 일수
 
-  const totalWeeks = Math.ceil((beforeRestDays + inMonth) / 7); // 총 몇 주가 필요한지
+  const totalWeeks = Math.ceil((beforeRestDays + totalDates) / 7); // 총 몇 주가 필요한지
   const totalCells = totalWeeks * 7; // 총 몇 칸이 필요한지
 
   // Grid 시작일 (이전달로 이동한 날짜)
