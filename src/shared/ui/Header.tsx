7f0676e23d969dbc15c5680ae17a35807ea3e2b3
beurmuz <fallinta2@gmail.com>
2025-10-21 00:07:08 +0900

    #10 [style] Header CSS 수정

--- src/shared/ui/Header.tsx
+++ src/shared/ui/Header.tsx
@@ -2,11 +2,11 @@ import calendarLogo from "@/assets/images/calendar_logo.svg";
 
 export default function Header() {
   return (
-    <header className="w-full flex items-center h-12 px-3 py-2  border-b border-gray-200 gap-1 ">
+    <header className="w-full flex items-center h-10 px-3 py-2  border-b border-gray-200 gap-1 ">
       <div>
-        <img src={calendarLogo} alt="Calendar Logo" className="h-5 w-5" />
+        <img src={calendarLogo} alt="Calendar Logo" className="h-4 w-4" />
       </div>
-      <h1 className="text-lg font-medium">Balendar</h1>
+      <h1 className="text-base font-medium">Balendar</h1>
     </header>
   );
 }
